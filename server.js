d512b3299aad6f0e2d66f8e4e5d8fd087f206d94
Philippe Leefsma <philippe.leefsma@autodesk.com>
2014-09-29 23:39:24 +0200

    embed fix
    embed fix

--- server.js
+++ server.js
@@ -29,10 +29,18 @@ var api = require('./routes/api');
 
 var app = express();
 
-var cors = function(req, res, next) {
+//CORS middleware
+var cors = function (req, res, next) {
+
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    res.header('Access-Control-Allow-Methods',
+        'GET,PUT,POST,DELETE');
+
+    res.header('Access-Control-Allow-Headers',
+            'Content-Type, Authorization, ' +
+            'Content-Length, X-Requested-With');
+
     next();
 }
 
