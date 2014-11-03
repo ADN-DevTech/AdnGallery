21997c2b31ac02a958b9519ace21074c7c29f2c9
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-11-03 19:20:07 +0100

    updated settings

--- add-ins/Autodesk.ADN.AddIns/Autodesk.ADN.RvtGalleryUploader/UserSettings.cs
+++ add-ins/Autodesk.ADN.AddIns/Autodesk.ADN.RvtGalleryUploader/UserSettings.cs
@@ -2,10 +2,8 @@
 {
   class UserSettings
   {
-    public readonly static string CONSUMER_KEY = "tAp1fqjjtcgqS4CKpCYDjAyNbKW4IVCC";
-    public readonly static string CONSUMER_SECRET = "q2LwUFg3MrYngc8l";
+    public readonly static string CONSUMER_KEY = "your_consumer_key";
+    public readonly static string CONSUMER_SECRET = "your_consumer_secret";
     public readonly static string BASE_URL = "https://developer.api.autodesk.com";
-    public readonly static string DEFAULT_USER_NAME = "Jeremy";
-    public readonly static string DEFAULT_EMAIL = "jeremy.tammik@eur.autodesk.com";
   }
 }
