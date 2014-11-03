f51f1b27e83dc16f9adce92c3cacd1813631f3a3
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-11-03 19:22:11 +0100

    updated

---
+++ add-ins/Autodesk.ADN.AddIns/Autodesk.ADN.RvtGalleryUploader/UserSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Autodesk.ADN.RvtGalleryUploader
+{
+  class UserSettings
+  {
+    public readonly static string CONSUMER_KEY = "your_consumer_key";
+    public readonly static string CONSUMER_SECRET = "your_consumer_secret";
+    public readonly static string BASE_URL = "https://developer.api.autodesk.com";
+  }
+}
