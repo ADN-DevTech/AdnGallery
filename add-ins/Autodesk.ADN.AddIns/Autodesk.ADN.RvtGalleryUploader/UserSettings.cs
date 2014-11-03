52bfd380479b2486a44774b133166fe6c28e50bf
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-11-03 19:16:01 +0100

    removed obsolete settings

--- add-ins/Autodesk.ADN.AddIns/Autodesk.ADN.RvtGalleryUploader/UserSettings.cs
+++ add-ins/Autodesk.ADN.AddIns/Autodesk.ADN.RvtGalleryUploader/UserSettings.cs
@@ -2,10 +2,6 @@
 {
   class UserSettings
   {
-    public readonly static string CONSUMER_KEY = "tAp1fqjjtcgqS4CKpCYDjAyNbKW4IVCC";
-    public readonly static string CONSUMER_SECRET = "q2LwUFg3MrYngc8l";
     public readonly static string BASE_URL = "https://developer.api.autodesk.com";
-    public readonly static string DEFAULT_USER_NAME = "Jeremy";
-    public readonly static string DEFAULT_EMAIL = "jeremy.tammik@eur.autodesk.com";
   }
 }
